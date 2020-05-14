65e81f7fa50b67146033cd2f222a1496e6ae7df1
João Pedro De Munno Durante <jaodurante@gmail.com>
2020-05-14 20:08:34 -0300

    chapter2 completed

--- chapter2/index.ts
+++ chapter2/index.ts
@@ -8,3 +8,26 @@
 
 let str = 'joao'
 str = 12                        // this line generates an error, if noEmitOnError == true -> index.js won't be generated
+
+
+/**
+ * target: javascript version ("es5", "es6", ...)
+ */
+
+ /**
+  * sourceMap: true ->generates a .js.map file that maps the js and ts files, in that way
+  * you can analysis and debug your .ts file in the browser
+  */
+
+/**
+ * outDir: directory where the .js files will be generated
+ */
+
+/**
+ * outFile: generate a single .js file for the entire application (it's not compatible with commonjs)
+ */
+
+ /**
+  * https://www.typescriptlang.org/docs/handbook/tsconfig-json.html
+  * https://www.typescriptlang.org/docs/handbook/compiler-options.html
+  */
