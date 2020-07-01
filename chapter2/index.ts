72596153bdc5de8aa13bf9b74400929dc82ef1b5
João Pedro De Munno Durante <jaodurante@gmail.com>
2020-07-01 20:05:23 -0300

    chapter2 commented

--- chapter2/index.ts
+++ chapter2/index.ts
@@ -6,8 +6,8 @@
   * noEmitOnError: true -> doesn't compile to JS when .ts file has an error (default is false)
  */
 
-let str = 'joao'
-str = 12                        // this line generates an error, if noEmitOnError == true -> index.js won't be generated
+//let str = 'joao'
+//str = 12                        // this line generates an error, if noEmitOnError == true -> index.js won't be generated
 
 
 /**
