e4c8c5b4de0b2821852c51e18bd85baa10571745
João Pedro De Munno Durante <jaodurante@gmail.com>
2020-07-01 20:04:49 -0300

    chapter5.2 completed

---
+++ chapter5/modules/area.ts
@@ -0,0 +1,9 @@
+const PI = 3.14
+
+export const circumferenceArea = (radius: number): number => {
+    return PI * radius * radius
+}
+
+export const rectangleArea = (height: number, width: number): number => {
+    return height * width
+}
--- chapter5/modules/index.ts
+++ chapter5/modules/index.ts
@@ -1,3 +1,9 @@
 /*******************************
  ***  CHAPTER 5.2 - MODULES  ***
  *******************************/
+
+ import { circumferenceArea, rectangleArea } from './area'
+
+ console.log('Module loaded!')
+ console.log(circumferenceArea(10))
+ console.log(rectangleArea(10, 2))
